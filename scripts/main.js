6be6cb471bc17f44dc2a4008223819043388cc1a
tybo-dev <tobias@diez.guru>
2022-08-06 14:52:27 +0200

    First EventListener Created

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World of JavaScript!'
+
+document.querySelector('html').addEventListener('click', function() {
+    alert('Ouch! Stop poking around like a dumbass...')
+})
