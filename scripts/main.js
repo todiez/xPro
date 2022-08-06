ebf127a4e5b747e1ef5db16f697d72fd4aaf7129
tybo-dev <tobias@diez.guru>
2022-08-06 14:58:50 +0200

    edited some syntax errors

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,4 @@ myHeading.textContent = 'Hello World of JavaScript!'
 
 document.querySelector('html').addEventListener('click', function() {
     alert('Ouch! Stop poking around like a dumbass...')
-})
+});
