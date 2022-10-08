616727f3e3a7440e57cef97cf812ddc533a9c9e6
tybo-dev <tobias@diez.guru git config --global user.name tgit config --global user.email tobias@diez.guru>
2022-10-09 01:17:13 +0200

    Real Time Bus Tracker done

--- module09/realTimeBusTracker/mapanimation.js
+++ module09/realTimeBusTracker/mapanimation.js
@@ -10,31 +10,78 @@ var map = new mapboxgl.Map({
   zoom: 12,
 });
 
-async function run() {
+const nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
+
+
+
+
+async function run1() {
   // get bus data
-  const locations = await getBusLocations();
+  const locations = await getBusLocations1();
   console.log(new Date());
   console.log(locations);
-  
+
+  //console.log(locationsOld);
+
   //create realTime busMarker of all busses on route and add to map (using mapbox library mapboxgl)
   for (let i = 0; i < locations.length; i++) {
-    let busMarker = new mapboxgl.Marker()
-        .setLngLat([
-        locations[i].attributes.longitude, 
+    let busMarker = new mapboxgl.Marker({ color: "red" })
+      .setLngLat([
+        locations[i].attributes.longitude,
         locations[i].attributes.latitude,
-        ])
-        .addTo(map);
-    }
+      ]).addTo(map);
+  }
+
+  const span1 = document.getElementById('span1');
+  span1.innerHTML = "   Currently are " + locations.length + " busses operating for route 1      "
+  
+  
+
+ 
   // timer
-  setTimeout(run, 15000);
+  setTimeout(run1, 15000);
+}
+
+async function run11() {
+  // get bus data
+  const locations = await getBusLocations11();
+  console.log(new Date());
+  console.log(locations);
+
+  //console.log(locationsOld);
+
+  //create realTime busMarker of all busses on route and add to map (using mapbox library mapboxgl)
+  for (let i = 0; i < locations.length; i++) {
+    let busMarker = new mapboxgl.Marker({ color: "red" })
+      .setLngLat([
+        locations[i].attributes.longitude,
+        locations[i].attributes.latitude,
+      ]).addTo(map);
+  }
+
+  
+  const span11 = document.getElementById('span11');
+  span11.innerHTML = "   Currently are " + locations.length + " busses operating for route 1      "
+  
+  // timer
+  setTimeout(run11, 15000);
 }
 
 // Request bus data from MBTA
-async function getBusLocations() {
+async function getBusLocations1() {
   const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
   const response = await fetch(url); //await the data to be fetched
   const json = await response.json(); //extract the data from the response
   return json.data; //return the data to the calling function
 }
 
-run();
+
+async function getBusLocations11() {
+  const url = "https://api-v3.mbta.com/vehicles?filter[route]=11&include=trip";
+  const response = await fetch(url); //await the data to be fetched
+  const json = await response.json(); //extract the data from the response
+  return json.data; //return the data to the calling function
+}
+
+
