826a35f8264a429c0413b33692c152cc2840e612
tybo-dev <tobias@diez.guru>
2022-12-14 12:56:30 +0100

    file structure updated

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-const myHeading = document.querySelector('h2');
-myHeading.textContent = 'Hello World of JavaScript!'
-
-document.querySelector('html').addEventListener('click', function() {
-    alert('Ouch! Stop poking around like a dumbass...')
-});
