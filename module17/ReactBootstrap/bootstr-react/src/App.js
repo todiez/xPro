81e3b461b1dbae386385e3d881c55402271c06c5
tybo-dev <tobias@diez.guru>
2022-12-16 13:57:45 +0100

    routes excercises done

--- module17/ReactBootstrap/bootstr-react/src/App.js
+++ module17/ReactBootstrap/bootstr-react/src/App.js
@@ -3,22 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+    <div className="card">
+      <div className="card-header">Featured</div>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">
+          With supporting text below as a natural lead-in to additional content.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href="#" className="btn btn-primary">
+          Go somewhere
         </a>
-      </header>
+      </div>
     </div>
+    <button>Test</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <div>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    </div>
+
+  </div>
   );
 }
 
