b6a07340d8136e44ed3aafa9fb3a4c3ad134b570
tybo-dev <tobias@diez.guru>
2023-01-12 18:56:01 +0100

    first context added

--- module18/context-basics/home.js
+++ module18/context-basics/home.js
@@ -1,8 +1,10 @@
 function Home(){
+    const ctx = React.useContext(UserContext);
     return (
         <div>
             <h3>Home Component</h3>
             <p>Welcome to the site - happy to see you</p>
+            {JSON.stringify(ctx.users)}
         </div>
     );
 }
