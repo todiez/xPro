c5a39060a318ea22565da96d7d234d9bd2a9625a
tybo-dev <tobias@diez.guru>
2023-02-16 17:09:54 +0100

    lowDB tested

--- module21/Server/simple.js
+++ module21/Server/simple.js
@@ -6,7 +6,14 @@ var db      = low(adapter);
 // init the data store
 // ---------------------------
 db.defaults({ posts: []}).write();
- 
+
+// Edit db.json content using native JS API
+db.get("posts").push({ id: 1, title: 'lowdb is awesome', publish: true }).write();
+db.get("posts").push({ id: 2, title: 'lowdb is 1', publish: true }).write();
+db.get("posts").push({ id: 3, title: 'lowdb is asome', publish: true }).write();
+db.get("posts").push({ id: 4, title: 'lowdb wesome', publish: true }).write();
+
+
 console.log(db.get('posts').value());
 
 // count posts
