41d2b3ce79af17524a78ab6c667f3caee936acf4
tybo-dev <tobias@diez.guru>
2023-03-10 10:08:40 +0100

    serving static files from express

--- module24/Serving Static Files/express-routes-master/index.js
+++ module24/Serving Static Files/express-routes-master/index.js
@@ -1,29 +1,31 @@
-const express = require('express')
+const express = require('express');
 
-var restaurants = [{id:0,name:"Woodshill"},{id:1,name:"Fiorellas"}]
+var restaurants = [
+  { id: 0, name: "Woodshill" },
+  { id: 1, name: "Fiorellas" },
+];
 
 const app = express();
 app.use(express.json());
 let options = {
-    dotfiles: "ignore",
-    redirect:false
-}
+  dotfiles: "ignore",
+  redirect: false,
+};
 
-app.use(express.static('public',options))
+app.use(express.static("public", options));
 
-app.get("/",(req,res)=>{
-let html = "<img src='img/tenor.gif'/>"
-let key = "<a href='.secret/key.txt'>secret key </a>"
-res.send(html+key)
-}
-)
-app.get("/restaurants", (req,res)=>{
-    res.send(restaurants);
-})
-
-app.post("/restaurant",(req,res)=>{
-    res.send(`${req.body.name} created`)
-})
+app.get("/", (req, res) => {
+  let html = "<img src='img/tenor.gif'/>";
+  let key = "<a href='.secret/key.txt'>secret key </a>";
+  res.send(html + key);
+});
+app.get("/restaurants", (req, res) => {
+  res.send(restaurants);
+});
 
 
-app.listen(4000,()=>console.log('Listening on 4000'))
+app.post("/restaurant", (req, res) => {
+  res.send(`${req.body.name} created`);
+});
+
+app.listen(4000, () => console.log("Listening on 4000"));
